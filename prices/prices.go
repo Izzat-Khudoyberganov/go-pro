d28a21e45269e2a28d11607ab2bfd238ec261020
izzat <izzat@gmail.com>
2024-05-12 10:33:49 +0500

    Added scanner for reading data from file

--- prices/prices.go
+++ prices/prices.go
@@ -1,12 +1,39 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrice        []float64
-	TaxIncludedPrices []float64
-	taxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("An error occured!")
+		fmt.Println(err)
+		return
+	}
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("Reading the content failed!")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
 }
 
 func (job TaxIncludedPriceJob) Proccess() {
